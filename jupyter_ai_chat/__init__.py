0ceffc71289c7f6e3718bbf76f82ef6fa998d551
fbalicchia <fbalicchia@cuebiq.com>
2024-02-01 11:30:55 +0100

    add new depdendency

--- jupyter_ai_chat/__init__.py
+++ jupyter_ai_chat/__init__.py
@@ -3,7 +3,13 @@ from ._version import __version__
 # expose engines and tasks on the module root so that they may be declared as
 # entrypoints in `pyproject.toml`
 from .myhandler import CustomChatHandler
+from .serverhandler import load_jupyter_server_extension
+from .serverhandler import AiExtensionChat
+
 
 def _jupyter_labextension_paths():
     return [{"src": "labextension", "dest": "@jupyter-ai/jupyter-ai-chat"}]
 
+
+def _jupyter_server_extension_points():
+    return [{"module": "jupyter_ai", "app": AiExtensionChat}]
