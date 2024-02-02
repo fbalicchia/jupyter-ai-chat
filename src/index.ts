91801cdbc0072790d99090207dfd4dadd186bdcc
fbalicchia <fbalicchia@cuebiq.com>
2024-02-02 11:49:00 +0100

    add plain cookiecutter

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import type { InsertionContext } from '@jupyter-ai/core';
+
 /**
  * Initialization data for the jupyter-ai-chat extension.
  */
@@ -12,6 +14,13 @@ const plugin: JupyterFrontEndPlugin<void> = {
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension jupyter-ai-chat is activated!');
 
+    // handles "test" insertion mode, which just shows output in a native
+    // browser alert.
+    app.commands.addCommand('ai:insert-test', {
+      execute: ((context: InsertionContext) => {
+        alert(context.response.output)
+      }) as any
+    })
   }
 };
 
